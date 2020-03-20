35ed5d887dd45e6666ad029abc296fca23a8551d
Tati Micheletti <tati.micheletti@gmail.com>
2020-03-19 22:57:16 -0700

    fitting and predicting functions for ordinal forest ok

--- R/HSImodelFit.R
+++ R/HSImodelFit.R
@@ -1,5 +1,28 @@
 HSImodelFit <- function(modLayers,
-                        IKLayer){
-  browser()
+                        IKLayer, subsetForModel = FALSE){
+  
+  if (isTRUE(subsetForModel)){
+    subsetForModel <- 50
+  } else {
+    if (any(!is(subsetForModel, "numeric"), isFALSE(subsetForModel))){
+      subsetForModel <- NULL
+    }
+  }
+  stk <- raster::stack(modLayers, IKLayer)
+  names(stk)[names(stk) == names(IKLayer)] <- "IKranks"
+  modelData <- data.table(getValues(stk))
+  modelData <- na.omit(modelData)
+  if (!is.null(subsetForModel)){
+    modelData <- suppressMessages(LandR::subsetDT(modelData, 
+                                                  by = "IKranks", 
+                                                  doSubset = subsetForModel))
+  }
+  modelData[, IKranks := as.factor(IKranks)]
+  
+  # With subsetForModel = 50, takes about 1.5 minutes
+  # With the full dataset it might be several hours.
+  HSImodel <- ordfor(depvar = "IKranks", data = modelData, nsets = 1000, ntreeperdiv = 100,
+                      ntreefinal = 5000, perffunction = "equal")
+  return(HSImodel)
 }
 
--- R/HSImodelPredict.R
+++ R/HSImodelPredict.R
@@ -1,7 +1,22 @@
 HSImodelPredict <- function(modelHSI,
                             modLayers,
                             currentTime,
-                            pathData,
                             pathOut){
-  browser()
+  
+  message(crayon::green(paste0("Predicting IK ranks for year ", currentTime)))
+  totalCellsLayers <- raster::ncell(modLayers)
+  modelData <- data.table(pixelID = 1:totalCellsLayers, getValues(modLayers))
+  modelData <- na.omit(modelData)
+  pixelID <- modelData$pixelID
+  modelData[, pixelID := NULL]
+  ranks <- predict(modelHSI, newdata = modelData) # Prediction for the NWT might take about an hour
+  rankValues <- data.table(pixelID = pixelID, ranks = as.numeric(ranks$ypred))
+  fullRankValues <- merge(data.table(pixelID = 1:totalCellsLayers), rankValues, on = "pixelID", all.x = TRUE)
+  setkey(fullRankValues, "pixelID")
+  predictedRankMap <- raster::setValues(modLayers[[1]], values = fullRankValues[["ranks"]])
+  writeRaster(predictedRankMap, file.path(pathOut, paste0("IK_basedHSI_year", currentTime)), 
+              format = "GTiff", overwrite = TRUE)
+  predictedRankMap <- raster::raster(file.path(pathOut, paste0("IK_basedHSI_year", 
+                                                               currentTime, ".tif")))
+  return(predictedRankMap)
 }
